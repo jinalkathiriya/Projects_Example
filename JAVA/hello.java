dc6234776a53dcca6fcbef4fdd88be7608fce02b
jinalkathiriya <135434782+jinalkathiriya@users.noreply.github.com>
2024-07-21 14:18:47 +0530

    Delete JAVA directory

--- JAVA/hello.java
+++
@@ -1,7 +0,0 @@
-class hello{
-  public static void main(String[] args) {
-    System.out.println("Welcome to Java");
-    System.out.println("Welcome to Computer Science");
-    System.out.println("Programming is fun");
-  }
-}
